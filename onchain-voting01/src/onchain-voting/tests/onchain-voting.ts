3829e1520e9af1ea08c85712268815eff91e864c
Hiroshi <infinith4@gmail.com>
2024-07-30 22:01:15 +0900

    Update onchain-voting.ts

--- onchain-voting01/src/onchain-voting/tests/onchain-voting.ts
+++ onchain-voting01/src/onchain-voting/tests/onchain-voting.ts
@@ -1,16 +1,51 @@
-import * as anchor from "@coral-xyz/anchor";
-import { Program } from "@coral-xyz/anchor";
+import * as anchor from "@project-serum/anchor";
+import { Program } from "@project-serum/anchor";
 import { OnchainVoting } from "../target/types/onchain_voting";
 
+
 describe("onchain-voting", () => {
-  // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
-
   const program = anchor.workspace.OnchainVoting as Program<OnchainVoting>;
+  let voteBank = anchor.web3.Keypair.generate();
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+
+  it("Creating vote bank for public to vote", async () => {
+    const tx = await program.methods.initVoteBank()
+      .accounts({
+        voteAccount: voteBank.publicKey,
+      })
+      .signers([voteBank])
+      .rpc();
+    console.log("TxHash ::", tx);
+  });
+
+
+  it("Vote for GM", async () => { 
+    const tx = await program.methods.gibVote({gm:{}})
+    .accounts({
+      voteAccount: voteBank.publicKey,
+    })
+    .rpc();
+    console.log("TxHash ::", tx);
+
+
+    let voteBankData = await program.account.voteBank.fetch(voteBank.publicKey);
+    console.log(`Total GMs :: ${voteBankData.gm}`)
+    console.log(`Total GNs :: ${voteBankData.gn}`)
+  });
+
+
+  it("Vote for GN", async () => { 
+    const tx = await program.methods.gibVote({g:{}})
+    .accounts({
+      voteAccount: voteBank.publicKey,
+    })
+    .rpc();
+    console.log("TxHash ::", tx);
+
+
+    let voteBankData = await program.account.voteBank.fetch(voteBank.publicKey);
+    console.log(`Total GMs :: ${voteBankData.gm}`)
+    console.log(`Total GNs :: ${voteBankData.gn}`)
   });
 });
