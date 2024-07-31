8ac269d6474c60f3403ebd5fba2df5e8e946aa1f
Hiroshi <infinith4@gmail.com>
2024-07-31 23:32:43 +0900

    m

--- self-custodial-facebook/src/programs/self-custodial-facebook/src/lib.rs
+++ self-custodial-facebook/src/programs/self-custodial-facebook/src/lib.rs
@@ -5,38 +5,46 @@ declare_id!("FmQCenBZQBc5f8EnF5zTik1mxxkNPwjTNEVJY4VWFZTE");
 #[program]
 pub mod self_custodial_facebook {
     use super::*;
+
     pub fn create_facebook(ctx: Context<Initialize>, name: String, status: String, twitter: String) -> Result<()> {
-        // setting userdata in user's account
         let users_account_data = &mut ctx.accounts.facebook_account;
-        users_account_data.bump = *ctx.bumps.get("facebook_account").unwrap();
-
-
-        users_account_data.authority = *ctx.accounts.signer.key;
-        users_account_data.name = name.to_owned();
-        users_account_data.status = status.to_owned();
-        users_account_data.twitter = twitter.to_owned();
-
-
-        // Printing User Info into program's on-chain transaction log.
-        msg!("Created a new account with following details 
-            Name :: {0}
-            Status :: {1}
-            Twitter :: {2}
-            ", name, status, twitter 
-        );
+        initialize_account(users_account_data, ctx.bumps.facebook_account.unwrap(), ctx.accounts.signer.key, &name, &status, &twitter)?;
+        log_account_creation(&name, &status, &twitter);
         Ok(())
     }
+
     pub fn update_status(ctx: Context<Update>, new_status: String) -> Result<()> {
         // Update user status, much more like whatsapp 24 hour status, without self destruction 😉
         msg!("Updating status from :: {0} -> to :: {1}", &ctx.accounts.facebook_account.status, &new_status);
         ctx.accounts.facebook_account.status = new_status;
+
         Ok(())
     }
+
     pub fn delete_account(_ctx: Context<Close>) -> Result<()> {
         msg!("Account Closed successfully");
         Ok(())
     }
 }
+
+fn initialize_account(account: &mut Account<FacebookAccount>, bump: &u8, authority: &Pubkey, name: &str, status: &str, twitter: &str) -> Result<()> {
+    account.bump = *bump;
+    account.authority = *authority;
+    account.name = name.to_string();
+    account.status = status.to_string();
+    account.twitter = twitter.to_string();
+    Ok(())
+}
+
+fn log_account_creation(name: &str, status: &str, twitter: &str) {
+    msg!("Created a new account with following details 
+        Name :: {0}
+        Status :: {1}
+        Twitter :: {2}
+        ", name, status, twitter 
+    );
+}
+
 #[derive(Accounts)]
 pub struct Initialize<'info> {
     // User's account
@@ -51,27 +59,8 @@ pub struct Initialize<'info> {
         seeds = ["self-custodial-facebook2".as_bytes(), signer.key().as_ref()], 
         bump,
     )] 
-    pub facebook_account: Account<'info, FacebookAccount>,
+    pub facebook_account: Box<Account<'info, FacebookAccount>>,
     pub system_program: Program<'info, System>,
-}
-#[account]
-pub struct FacebookAccount {
-    pub authority: Pubkey, // Authority of this account
-    pub bump: u8,
-    pub name: String, // Max 10 Chars
-    pub status: String, // Max 100 Chars
-    pub twitter: String // Max 10 Chars
-}
-
-
-impl FacebookAccount {
-    const LEN: usize = 
-        8 + // discriminator
-        32 + // Pubkey
-        1 + // bump
-        (4 + 10) + // 10 chars of Name
-        (4 + 100) + // 100 chars of Status  
-        (4 + 10); // 10 chars' twitter
 }
 
 #[derive(Accounts)]
@@ -84,7 +73,7 @@ pub struct Update<'info> {
         seeds = ["self-custodial-facebook".as_bytes(), signer.key().as_ref()], 
         bump = facebook_account.bump,
     )]
-    pub facebook_account: Account<'info, FacebookAccount>,
+    pub facebook_account: Box<Account<'info, FacebookAccount>>,
 }
 
 #[derive(Accounts)]
@@ -98,5 +87,24 @@ pub struct Close<'info> {
         bump = facebook_account.bump,
         close=signer
     )]
-    pub facebook_account: Account<'info, FacebookAccount>,
+    pub facebook_account: Box<Account<'info, FacebookAccount>>,
+}
+
+#[account]
+pub struct FacebookAccount {
+    pub authority: Pubkey, // Authority of this account
+    pub bump: u8,
+    pub name: String, // Max 10 Chars
+    pub status: String, // Max 50 Chars
+    pub twitter: String // Max 10 Chars
+}
+
+impl FacebookAccount {
+    const LEN: usize = 
+         8 + // discriminator
+        32 + // Pubkey
+        1 + // bump
+        (4 + 10) + // 10 chars of Name
+        (4 + 50) + // 50 chars of Status  
+        (4 + 10); // 10 chars' twitter
 }
